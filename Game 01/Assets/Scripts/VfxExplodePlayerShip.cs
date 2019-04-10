1cb6004d48cd2fbce1b08c63fa489978e14e5188
David Van Hoecke <davidvanhoecke@gmail.com>
2019-04-10 23:18:42 +0200

    Added animated models

---
+++ Game 01/Assets/Scripts/VfxExplodePlayerShip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VfxExplodePlayerShip : MonoBehaviour
+{
+    private Animator[] anim;
+    private Animator mainAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mainAnim = GetComponent<Animator>();
+        //mainAnim.Play("explode");
+        anim = GetComponentsInChildren<Animator>();
+        anim.ToList().ForEach(animator =>
+        {
+            var tmp = animator.GetCurrentAnimatorStateInfo(0);
+            var tmp2 = animator.runtimeAnimatorController.animationClips[0];
+            
+            animator.updateMode = AnimatorUpdateMode.Normal;
+            animator.Play("explode");
+            //animator.Play(tmp);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
