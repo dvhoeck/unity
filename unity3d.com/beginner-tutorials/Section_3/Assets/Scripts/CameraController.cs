91216518603e660cf56a48e071f05e32fabe8a72
David Van Hoecke <david.vanhoecke@delair-tech.com>
2019-04-22 18:35:58 +0200

    Added tutorial from unity3d.com
    Sections 1 to 3, and section 4 partly

---
+++ unity3d.com/beginner-tutorials/Section_3/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + _offset;
+    }
+}
---
+++ unity3d.com/beginner-tutorials/Section_3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 1;
+    public int Count;
+    public Text CountText;
+    public Text WinText;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Count = 0;
+        SetCountText();
+
+        WinText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pick Up")
+        {
+            other.gameObject.SetActive(false);
+            Count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + Count.ToString();
+
+        if(Count == 12)
+            WinText.text = "You win!";
+    }
+}
